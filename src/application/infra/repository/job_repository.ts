17fdfd9a168722bb395f3211306dbc153e8f2c97
Mohamed Aljoke <mohamed@idip.com.br>
2024-06-23 13:53:40 -0300

    fix: broken build

--- src/application/infra/repository/job_repository.ts
+++ src/application/infra/repository/job_repository.ts
@@ -10,13 +10,13 @@ export interface IJobRepository {
 export class JobRepositoryDatabase implements IJobRepository {
   constructor(readonly dbConnection: IDatabaseConnection) {}
   async createJob(job: Job): Promise<Job> {
-    const { companyId, title, description, status } = job;
+    const { companyId, title, description } = job;
     const query = `
     INSERT INTO jobs (company_id, title, description, status) 
     VALUES ($1, $2, $3, $4) 
     RETURNING *;
     `;
-    const values = [companyId, title, description, status];
+    const values = [companyId, title, description, job.getStatus()];
     const jobDB = (await this.dbConnection.query(query, values))[0];
     return new Job(
       jobDB.id,
